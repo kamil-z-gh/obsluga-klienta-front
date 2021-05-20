fe8046b7f52df0f83f84e00920b322fec72176e0
Kamil Żurad <kamil.zurad@gmail.com>
2021-05-20 18:21:35 +0200

    basic setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,22 @@
-import '../styles/globals.css'
-
-import { Provider } from 'react-redux'
-import type { AppProps } from 'next/app'
-
-import store from '../app/store'
+import { Provider } from "react-redux";
+import type { AppProps } from "next/app";
+import CreateGlobalStyle from "styles/globalStyles.styled";
+import store from "../app/store";
+import { StylesProvider, createMuiTheme } from "@material-ui/core/styles";
+import { ThemeProvider as StyledThemeProvider } from "styled-components";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const theme = createMuiTheme();
   return (
     <Provider store={store}>
-      <Component {...pageProps} />
+      <StylesProvider injectFirst>
+        <StyledThemeProvider theme={theme}>
+          <CreateGlobalStyle />
+          <Component {...pageProps} />
+        </StyledThemeProvider>
+      </StylesProvider>
     </Provider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
